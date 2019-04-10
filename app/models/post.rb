343e7e5d52d27a798f732a8e97d8ec1b86f52526
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-04-10 09:34:14 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+require "pry"
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbaity_title
+
+  def clickbaity_title
+    return if !self.title
+
+    if !self.title.include?("Won't Believe") && !self.title.include?("Secret") && !self.title.match(/Top \d+/) && !self.title.include?("Guess")
+
+       errors.add(:title, "Title isn't clickbaity")
+
+     end
+  end
+
 end
